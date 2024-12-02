7bb5eb42fb44e538aa454a954fa4d7a4aae0539d
Aleksandra Indulska <s24477@pjwstk.edu.pl>
2024-12-02 22:06:15 +0100

    feat: create custom docker image

--- airflow-p4/dags/notify.py
+++ airflow-p4/dags/notify.py
@@ -11,8 +11,9 @@ def notify_failure(context):
     # Create the message with the name of the failed DAG and failed task
     teams_failure_message.title("Airflow DAG Failed")
     teams_failure_message.text(
-        f"DAG: {context['task_instance'].dag_id}\nTask: {context['task_instance'].task_id}"
+        f"DAG: {context['task_instance'].dag_id} \n Task: {context['task_instance'].task_id} \n [Check run for more details](http://localhost:8080/dags/create_model/grid?tab=graph&dag_run_id={context['run_id']})"
     )
+
     teams_failure_message.color("ff0000")
 
     # Send the message
@@ -27,7 +28,7 @@ def notify_success(context):
 
     teams_success_message.title("Airflow DAG Succeeded")
     teams_success_message.text(
-        f"DAG: {context['task_instance'].dag_id}\nTask: {context['task_instance'].task_id}"
+        f"DAG: {context['task_instance'].dag_id} \n Task: {context['task_instance'].task_id}"
     )
     teams_success_message.color("00ff00")
 
